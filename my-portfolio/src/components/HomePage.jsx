0a5c160cea23bad549217cc6d528be6d3596b62a
eoghan523 <eoghan523@gmail.com>
2024-11-27 22:28:08 +0000

    created the Header.jsx and HomePage

---
+++ my-portfolio/src/components/HomePage.jsx
@@ -0,0 +1,23 @@
+// src/components/Homepage.jsx
+import React from 'react';
+
+const Homepage = () => (
+  <section>
+    <h1>Welcome to My Portfolio</h1>
+    <p>My name is Eoghan, a junior developer. I am currently on a full stack development dootcamp where is I have learned coding languages and scripts.</p>
+    <div>
+      <h2>Skills</h2>
+      <ul>
+        <li>React</li>
+        <li>Django</li>
+        <li>CSS/HTML</li>
+        <li>Node.js</li>
+        <li>Javascript</li>
+        <li>Python</li>
+        
+      </ul>
+    </div>
+  </section>
+);
+
+export default Homepage;
