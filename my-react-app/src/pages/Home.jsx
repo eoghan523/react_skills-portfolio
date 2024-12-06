960e29bc915c2e0977ab632c82db8c82da7a8da5
eoghan523 <eoghan523@gmail.com>
2024-12-06 18:04:55 +0000

    corrected typos on the home bio.

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -2,8 +2,8 @@ const Home = () => {
     return (
       <div>
         <h1>Welcome to My Portfolio</h1>
-        <p>My name is Eoghan. I am a junior web developer and havee created this react web app as a showcase for my github repository.
-            Feel free to explore this website and checj the project links. </p>
+        <p>My name is Eoghan. I am a Junior web developer and haee created this react web app as a showcase for my github repository.
+            Feel free to explore this website and check the project links. </p>
       </div>
     );
   };
